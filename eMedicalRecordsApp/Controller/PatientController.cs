e4a31a7afdbb7620a79ebc9aebb86a5b4008ad49
Sh3ldoris <adam.lany@gmail.com>
2022-01-25 15:52:02 +0100

    Patient controller setup

---
+++ eMedicalRecordsApp/Controller/PatientController.cs
@@ -0,0 +1,46 @@
+using eMedicalRecordsApp.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eMedicalRecordsApp.Controller;
+
+[ApiController]
+[Route("patients")]
+public class PatientController : ControllerBase
+{
+    
+    [HttpGet]   
+    public IActionResult AllPatients()
+    {
+        return Ok(Enumerable.Empty<Patient>());
+    }
+
+    [HttpGet("{code}")]
+    public IActionResult PatientByCode(string code)
+    {
+        return Ok(Enumerable.Empty<Patient>());
+    }
+
+    [HttpPost("doctor")]
+    public IActionResult PatientByDoctor(Doctor doctor)
+    {
+        return Ok(Enumerable.Empty<Patient>());
+    }
+    
+    [HttpGet("filtered")]
+    public IActionResult PatientByFilter(PatientFilter filter)
+    {
+        return Ok(Enumerable.Empty<Patient>());
+    }
+
+    [HttpPost]
+    public IActionResult SavePatient(Patient patient)
+    {
+        return Accepted(Enumerable.Empty<Patient>());
+    }
+    
+    [HttpPut]
+    public IActionResult UpdatePatient(Patient patient)
+    {
+        return Ok(Enumerable.Empty<Patient>());
+    }
+}
---
+++ eMedicalRecordsApp/Model/PatientFilter.cs
@@ -0,0 +1,9 @@
+namespace eMedicalRecordsApp.Model;
+
+public class PatientFilter
+{
+    public string Address { get; set; }
+    public string BirthNumber { get; set; }
+    public string Name { get; set; }
+    public string LastName { get; set; }
+}
